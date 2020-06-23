bdffb7f4b564b5ba5ec21da8103e2c3e8f9aef8a
fesiib <tlekbay.b@gmail.com>
2020-06-24 04:51:53 +0900

    Plugin v2.0

--- priora-plugin/src/main/java/io/jenkins/plugins/sample/ScriptPython.java
+++ priora-plugin/src/main/java/io/jenkins/plugins/sample/ScriptPython.java
@@ -15,12 +15,45 @@ public class ScriptPython {
       this.pb = null;
    }
 
-   public List<String> runScript(List<String> args, String method, Double[] weights, String scriptFile, TaskListener listener) throws InterruptedException, IOException {
+   public static List<String> readStream(InputStream out) throws IOException {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(out, StandardCharsets.UTF_8));
+      String line = null;
+      List<String> ret = new ArrayList<>();
+      try{
+         while ((line = reader.readLine()) != null) {
+            ret.add(line);
+         }
+         reader.close();
+         out.close();
+      } catch(IOException e){
+         throw e;
+      }
+      return ret;
+   }
+
+   public List<String> runScript(List<String> tests, String method, Double[] weights, String scriptFile, TaskListener listener) throws InterruptedException, IOException {
+      String path = System.getProperty("user.dir") + "/src/main/java/io/jenkins/plugins/sample/algorithm/";
+      try {
+         FileOutputStream fis = new FileOutputStream(path + "tests_info.txt", false);
+         OutputStreamWriter writer = new OutputStreamWriter(fis, StandardCharsets.UTF_8);   
+         for (int i = 0; i < tests.size(); i++) {
+            writer.write(tests.get(i) + System.lineSeparator());
+         }
+         writer.flush();
+         writer.close();
+         fis.close();
+      } catch(IOException e){
+         throw e;
+      } catch(Throwable e) {
+         e.printStackTrace(listener.getLogger());
+      }
+
+      List<String> args = new ArrayList<>();
       for (int i = 0; i < weights.length; i++) {
          args.add(0, weights[i].toString());
       }
       args.add(0, method);
-      String path = System.getProperty("user.dir") + "/src/main/java/io/jenkins/plugins/sample/algorithm/";
+      args.add(0, path);
       args.add(0, path + scriptFile + ".py");
       args.add(0, "python3");
 
@@ -29,7 +62,7 @@ public class ScriptPython {
       Process process = null;
       try{
          pb = new ProcessBuilder(args);
-         listener.getLogger().println("command: " + pb.command());
+         //listener.getLogger().println("command: " + pb.command());
          process = pb.start();
       } catch(IOException e) {
          listener.getLogger().println("Exception IO in pb.start");
@@ -41,20 +74,31 @@ public class ScriptPython {
          listener.getLogger().println("Could not execute the Evaluation");
          return new ArrayList<>();
       }
-      InputStream stdout = process.getInputStream();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(stdout, StandardCharsets.UTF_8));
-      String line;
-      List<String> ret = new ArrayList<>();
-      try{
-         while ((line = reader.readLine()) != null) {
-            ret.add(line);
+
+      List<String> ret = null;
+      try {
+         int ext = process.waitFor();
+         listener.getLogger().println("Algorithm ended with exit code:" + ext);
+         if (ext != 0) {
+            try {
+               ret = readStream(process.getErrorStream());
+               for (int i = 0; i < ret.size(); i++) {
+                  listener.getLogger().println(ret.get(i));
+               }
+            } catch(IOException e) {
+               throw e;
+            }
          }
-         reader.close();
-         stdout.close();
-      } catch(IOException e){
-         listener.getLogger().println("Exception in reading output"+ e.toString());
+         else {
+            try {
+               ret = readStream(new FileInputStream(path + "tests_ordering.txt"));
+            } catch(IOException e) {
+               throw e;
+            }
+         }
+      } catch(InterruptedException e) {
          throw e;
-      }
+      } 
       return ret;
    }
 }
--- priora-plugin/src/main/java/io/jenkins/plugins/sample/algorithm/evaluation.py
+++ priora-plugin/src/main/java/io/jenkins/plugins/sample/algorithm/evaluation.py
@@ -34,51 +34,104 @@ def select_from_front(front, w1, w2, w3):
     return front[index]
 
 def main(argv):
-    #algo, w1, w2, w3 = parse_arguments()
-    algo = argv[0]
-    w1, w2, w3 = float(argv[1]), float(argv[2]), float(argv[3])
+    path = argv[0]
+    algo = argv[1]
+    w1, w2, w3 = float(argv[2]), float(argv[3]), float(argv[4]) 
     tests_info = {}
     tests_name ={}
     tests = []
     k = 0
-    #file = 'tests_info.txt'
-    for buf in argv[4:]:
-        each_name = buf.split()[0]
-        each = list(map(lambda x: int(x), buf.split()[1:]))
-        if each[2] == 0:
-            each[0] = 0
-        else:
-            each[0] = each[0]/each[2]
-        tests_name[k] = each_name
-        tests_info[k] = each
-        tests.append(k)
-        k += 1
+    file = path + 'tests_info.txt'
+    with open(file, 'r') as f:
+        while True:
+            buf = f.readline()
+            if not buf:
+                break
+            each_name = buf.split()[0]
+            each = list(map(lambda x: int(x), buf.split()[1:]))
+            if each[2] == 0:
+                each[0] = 0
+            else:
+                each[0] = each[0]/each[2]
+            tests_name[k] = each_name
+            tests_info[k] = each
+            tests.append(k)
+            k += 1
+    fout = path + 'tests_ordering.txt'
     if k == 0:
+        f = open(fout, 'w')
+        f.close()
+        return 
+    if k == 1:
+        f = open(fout, 'w')
+        f.write(str(tests_name[0]) + '\n')
+        f.close()
         return
-    if algo=='NSGA' and k > 2:
+    if k > 2 and algo=='NSGA':
         genalg = GA.GeneticAlgorithm(200,10,300,0.3, tests, tests_info)
         front, history = genalg.generate_solution()
         ret = select_from_front(front, w1, w2, w3)
-        #fout = 'tests_ordering_nsga.txt'
-        #with open(sys.stdout, 'w') as f:
-        for i in range (len(ret.order)):
-            test_name = tests_name[ret.order[i]]
-            #row = "{} {}".format(test_name,i)
-            #f.write(str(row) + '\n')
-            print(test_name)
-        #    f.close()
+        with open(fout, 'w') as f:
+            for i in range (len(ret.order)):
+                test_name = tests_name[ret.order[i]]
+                #row = "{} {}".format(test_name,i)
+                f.write(str(test_name) + '\n')
+            f.close()
         return ret
-    elif k < 3 or algo=="Greedy":
+    elif algo=="Greedy" or k < 3:
         order = greedy.sort(tests_info, w1, w2, w3)
-        #fout = 'tests_ordering_greedy.txt'
-        #with open(sys.stdout, 'w') as f:
-        for i in range (len(order)):
-            test_name = tests_name[order[i][0]]
-            #row = "{} {}".format(test_name,i)
-            #f.write(str(row) + '\n')
-            print(test_name)
-        #    f.close()
+        with open(fout, 'w') as f:
+            for i in range (len(order)):
+                test_name = tests_name[order[i][0]]
+                #row = "{} {}".format(test_name,i)
+                f.write(str(test_name) + '\n')
+            f.close()
         return
+    # #algo, w1, w2, w3 = parse_arguments()
+    # algo = argv[0]
+    # w1, w2, w3 = float(argv[1]), float(argv[2]), float(argv[3])
+    # tests_info = {}
+    # tests_name ={}
+    # tests = []
+    # k = 0
+    # #file = 'tests_info.txt'
+    # for buf in argv[4:]:
+    #     each_name = buf.split()[0]
+    #     each = list(map(lambda x: int(x), buf.split()[1:]))
+    #     if each[2] == 0:
+    #         each[0] = 0
+    #     else:
+    #         each[0] = each[0]/each[2]
+    #     tests_name[k] = each_name
+    #     tests_info[k] = each
+    #     tests.append(k)
+    #     k += 1
+    # if k == 0:
+    #     return
+    # if algo=='NSGA' and k > 2:
+    #     genalg = GA.GeneticAlgorithm(200,10,300,0.3, tests, tests_info)
+    #     front, history = genalg.generate_solution()
+    #     ret = select_from_front(front, w1, w2, w3)
+    #     #fout = 'tests_ordering_nsga.txt'
+    #     #with open(sys.stdout, 'w') as f:
+    #     for i in range (len(ret.order)):
+    #         test_name = tests_name[ret.order[i]]
+    #         #row = "{} {}".format(test_name,i)
+    #         #f.write(str(row) + '\n')
+    #         print(test_name)
+    #     #    f.close()
+    #     return ret
+    # elif k < 3 or algo=="Greedy":
+    #     order = greedy.sort(tests_info, w1, w2, w3)
+    #     #fout = 'tests_ordering_greedy.txt'
+    #     #with open(sys.stdout, 'w') as f:
+    #     for i in range (len(order)):
+    #         test_name = tests_name[order[i][0]]
+    #         #row = "{} {}".format(test_name,i)
+    #         #f.write(str(row) + '\n')
+    #         print(test_name)
+    #     #    f.close()
+    #     return
 
 # def parse_arguments():
 #     parser = argparse.ArgumentParser()
@@ -90,3 +143,5 @@ def main(argv):
 #     return args.a, args.w1, args.w2, args.w3
 if __name__ == "__main__":
     main(sys.argv[1:])
+
+    
